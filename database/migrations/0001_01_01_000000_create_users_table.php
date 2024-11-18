d2c2f685e94274205c7fb72f561e370fa155c761
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-11-18 17:13:42 +0700

    [EDITED] Add point to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
               ]
               );
             $table->enum('role',['super']);
+            $table->integer('point');
             $table->timestamps();
         });
 
